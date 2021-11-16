e9bec5ccba7c270ad8a33b24664d53aee8d9e98e
Adam Pham <thaihoangpham2008@gmail.com>
2021-11-16 23:51:32 +0800

    find all categories function

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,6 +6,14 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: { model: Product }
+  })
+    .then(dbCategoryData => res.json(dbCategoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.get('/:id', (req, res) => {

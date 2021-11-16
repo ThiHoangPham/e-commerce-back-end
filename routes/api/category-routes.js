6efaa59464ad7d70c61c5d1adc3827bbf95c4eb6
Adam Pham <thaihoangpham2008@gmail.com>
2021-11-17 01:16:10 +0800

    fixed routes issues

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const { DataTypes } = require('sequelize/types');
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint

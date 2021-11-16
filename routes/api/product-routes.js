715fb0ba3f88f0bf683d7d5d1dcee34f1e8e40e1
Adam Pham <thaihoangpham2008@gmail.com>
2021-11-17 00:27:19 +0800

    router.get product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,6 +7,17 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [
+      { model: Category },
+      { model: Tag }
+    ]
+  })
+    .then(dbProductData => res.json(dbProductData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // get one product

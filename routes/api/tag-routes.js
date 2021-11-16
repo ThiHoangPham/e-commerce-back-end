f9dae94c40a16b74066590e2c6485d54611d3d9b
Adam Pham <thaihoangpham2008@gmail.com>
2021-11-17 00:48:43 +0800

    router.get/id tag-routes.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -19,6 +19,21 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    where: { id: req.params.id },
+    include: { model: Product }
+  })
+    .then(dbTagData => {
+      if (!dbTagData) {
+        res.status(404).json({ message: 'No tag found with this id' });
+        return;
+      }
+      res.json(dbTagData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.post('/', (req, res) => {

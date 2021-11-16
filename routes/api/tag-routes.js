f0d50231049987313816413778c151e150dbd798
Adam Pham <thaihoangpham2008@gmail.com>
2021-11-17 00:47:16 +0800

    router.get tag-routes.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,6 +6,14 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    include: { model: Product }
+  })
+    .then(dbTagData => res.json(dbTagData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.get('/:id', (req, res) => {
